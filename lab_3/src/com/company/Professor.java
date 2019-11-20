3320299960e7e70de85c23cc858b09b592c43a2f
Пожилой молодой человек <49003989+Kladmen228@users.noreply.github.com>
2019-11-20 18:36:10 +0300

    upd Professor

--- lab_3/src/com/company/Professor.java
+++ lab_3/src/com/company/Professor.java
@@ -1,11 +1,8 @@
 package com.company;
 
-class Professor extends  User{
-    public Professor(String Name, String Login, String Password) {
-        super(Name, Login, Password);
-    }
-
-    public void takeResult(Request request, int room, String dayOfWeek, int time){
-
+class Professor extends User
+{
+    Professor(String name, String login, String password) {
+        super(name, login, password);
     }
 }
