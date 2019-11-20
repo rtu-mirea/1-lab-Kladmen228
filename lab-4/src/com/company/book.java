5e87e01dbced8817e839a9893f928b09ef21e664
Пожилой молодой человек <49003989+Kladmen228@users.noreply.github.com>
2019-11-20 21:01:01 +0300

    add book class

---
+++ lab-4/src/com/company/book.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class book {
+    private String Author;
+    private String Name;
+    private String Number;
+    private String Publisher;
+    private int Count;
+    private int Cost;
+
+    private String getNumber(String name, String author){
+        return Number;
+    }
+
+    private String checkBooks(){
+        if(true)
+            return "Книги изданы в 1 издании";
+        else
+            return "Книги не изданы в 1 издании";
+    }
+
+    private void authorBooks(String author){
+        print(" ");
+    }
+
+    private void changePublisher(String pub1, String pub2){
+        print("");
+    }
+    
+    private void print(String txt){
+        System.out.println(txt);
+    }
+}
