60022f12887a417917b7352ee875ddbcf4e32ef4
Пожилой молодой человек <49003989+Kladmen228@users.noreply.github.com>
2019-11-26 22:47:27 +0300

    upload lab-5

---
+++ lab-5/src/com/company/Timetable.java
@@ -0,0 +1,28 @@
+package com.company;
+import java.util.ArrayList;
+
+public class Timetable
+{
+    public static ArrayList<Professor> users;
+    public static ArrayList<Request> requests;
+    public static Pairs[][] pairs;
+    public static Professor currentUser;
+    public static int rooms;
+    public static int groups;
+
+    public static void main(String[] args) {
+        LoginWindow loginWindow = new LoginWindow();
+        loginWindow.setVisible(true);
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 6; ++j) {
+                Timetable.pairs[i][j] = new Pairs();
+            }
+        }
+    }
+
+    static {
+        Timetable.users = new ArrayList<>();
+        Timetable.requests = new ArrayList<>();
+        Timetable.pairs = new Pairs[6][6];
+    }
+}
